8f83f99e49c227e16577c5541c78a9cf36dd251a
capricorn <an.anurag@msn.com>
2019-10-01 20:42:10 +0530

    5min added

---
+++ 5min/anotations.js
@@ -0,0 +1,14 @@
+// annotations
+function anotations(person) {
+    return "Hello, " + person;
+}
+var user = "Anurag";
+var result = anotations(user);
+alert(result);
+// type annotations
+function greeter(person) {
+    return "Hello, " + person;
+}
+// user = [0, 1, 2];
+result = greeter(user);
+alert(result);
